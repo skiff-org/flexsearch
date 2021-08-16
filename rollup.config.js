607eee3982d0407807c589bd29d9d347aaf31d3e
Andrew Lee <andrew.lee@nightwatchapp.com>
2021-08-16 14:44:09 -0400

    chore: add rollup terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 // Based on Rollup example app
 // https://github.com/rollup/rollup-starter-lib/blob/88a2d55d449e2cb12191199d993c08bc07c90be2/rollup.config.js
 import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
 
-export default [
-  {
-    input: 'src/index.js',
-    output: pkg.browser,
+export default {
+  input: 'src/index.js',
+  output: {
+    name: pkg.name,
+    file: pkg.browser,
     format: 'umd'
-  }
-]
+  },
+  plugins: terser()
+}
+
